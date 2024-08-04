7fd2152aa6ce7d7caa1ce78cdcbebc40e53ad6b3
mostafa <manfscience@gmail.com>
2024-08-04 08:33:19 +0300

    making app tokens unique using redis increment

--- app/controllers/chat_apps_controller.rb
+++ app/controllers/chat_apps_controller.rb
@@ -1,7 +1,15 @@
+require 'time'
+require 'digest'
 class ChatAppsController < ApplicationController
+
+    MAIN_INC_KEY = "app_inc_key"
+
+
     def create
         begin
-            token = (0...20).map { ('a'..'z').to_a[rand(26)] }.join #random at first then fix it
+            Rails.cache.write(MAIN_INC_KEY, 0, :raw => true,:unless_exist => true, :expires_in => 999999)
+            token = Digest::MD5.hexdigest(Rails.cache.increment(MAIN_INC_KEY, 1, :raw => true).to_s + Time.now().to_s)
+            
             app = ChatApp.new(name: params[:name], token: token)
             app.save
             render json: app.as_json(except: [:id, :created_at, :updated_at])
