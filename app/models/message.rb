0729e883677eb0541ee6c151776abc390008526d
mostafa <manfscience@gmail.com>
2024-08-05 14:25:10 +0300

    adding elastic search + the query required to search messages in the message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :chat
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  def search_messages(search_str, chat_id)
+    Message.search({"query": {"bool": {"must": {"query_string": {"query": search_str,"default_field": "text"}},"filter": {"term": {"chat_id": chat_id.to_i}}}}})
+  end
+  
 end
