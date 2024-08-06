79470c0e90f63f59d7bf5c16d550e92cc622264a
mostafa <manfscience@gmail.com>
2024-08-06 10:01:55 +0300

    add callback to increment and decrement chat count

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,18 @@
 class Message < ApplicationRecord
   belongs_to :chat
+  after_commit :increment_chat, on: :create
+  after_commit :decrement_chat, on: :destroy
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+  
+  # we can make an action job to preform this update periodically with whenever
+  #but honestly i find that solution neater
+  def increment_chat
+    self.chat.increment!(:chat_count)
+  end
+  def decrement_chat
+    self.chat.decrement!(:chat_count)
+  end
 
   def self.search_messages(chat_id, search_str)
     Message.search({"query": {"bool": {"must": {"query_string": {"query": search_str,"default_field": "text"}},"filter": {"term": {"chat_id": chat_id.to_i}}}}, sort:["number"]})
